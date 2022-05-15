8f602952aabf640eb04de456b3e436ce3f5e5f7f
Shafat Alam <61838416+Shafat21@users.noreply.github.com>
2022-05-15 15:24:35 +0600

    s

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping Command',
-    execute(message, args){
-        message.channel.send('Pong');
-    }, 
-};
--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const fs = require("fs");
-const config = require('./config.json');
-const BOT_TOKEN = config.BOT_TOKEN;
-const PREFIX = config.PREFIX;
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-let PlayerCount = require('./server/players');
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-    
-    setInterval(() => {
-      PlayerCount.getPlayerCount().then((result) => {
-          client.user.setActivity(`with ${result.data.length} players`,{ type: 'PLAYING' });
-      })
-    }, 10000);
+const { Client, Collection, Intents } = require("discord.js");
+const client = new Client({
+    allowedMentions: { parse: ['users', 'roles'] },
+    fetchAllMembers: false,
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS ],
 });
 
-client.login(BOT_TOKEN);
+//SET COLLECTION
+client.commandes = new Collection();
+client.slash = new Collection();
+client.aliases = new Collection();
+cooldowns = new Collection();
 
-for(const file of commandFiles){
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
+//SET UTILS
+client.logger = require('./src/utils/logger');
+client.color = require('./src/utils/color.js');
 
-client.on('message', message =>{
-  if(!message.content.startsWith(PREFIX) || message.author.bot) return
-  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
+//SET CONFIG
+client.config = require('./config');
 
-  if(!client.commands.has(command)) return
-  try {
-    client.commands.get(command).execute(message, args);
-  } catch (error) {
-    console.error(error);
-  }
-});
+// LOAD THE 4 HANDLERS
+["error", "command", "slashCommands", "event"].forEach(file => { require(`./src/utils/handlers/${file}`)(client) })
+
+client.login(client.config.token); 
